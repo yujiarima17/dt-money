0577b8a98d1065a04bb9092729f37e483bfd3b60
Yuji Arima <arimayuji2@gmail.com>
2023-11-11 18:21:47 -0300

    transaction modal form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 
 import logoImg from '../../assets/logo.svg'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import { NewTransactionModal } from '../NewTransactionModal'
+import * as Dialog from '@radix-ui/react-dialog';
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                 <Dialog.Trigger asChild>
+                      <NewTransactionButton>Nova transação</NewTransactionButton>
+                 </Dialog.Trigger>
+                 <NewTransactionModal/>
+                </Dialog.Root>
+                
             </HeaderContent>
         </HeaderContainer>
     )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,16 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
+
     return(
         <div>
             <Header/>
             <Summary/>
          <TransactionsContainer>
+            <SearchForm/>
             <TransactionsTable>
                 <tbody>
                     <tr>
